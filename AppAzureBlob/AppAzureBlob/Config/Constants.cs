5235c8ca7136790f472e3d2e9dd88159cf4011eb
Diego Alberto León <diego.alberto.leon.lopez@gmail.com>
2020-07-20 15:08:57 -0500

    updated

--- AppAzureBlob/AppAzureBlob/Config/Constants.cs
+++ AppAzureBlob/AppAzureBlob/Config/Constants.cs
@@ -7,6 +7,6 @@ namespace AppAzureBlob.Config
     public class Constants
     {
         public const string StorageConnection =
-            "DefaultEndpointsProtocol=https;AccountName=xamarinformsapps;AccountKey=cTepiSk+dILu5LwGprGFGK4hCm74Xxv29ajYZlBIh3/nSAFVDNXzYtJP9DpqmDdy6vaMHMoXQkozHxgGyjP2Nw==;EndpointSuffix=core.windows.net";
+            "DefaultEndpointsProtocol=https;AccountName=xamarinformsapps;AccountKey=your_key;EndpointSuffix=core.windows.net";
     }
 }
